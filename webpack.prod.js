394e9a6b906e125275e426fd5ad6dc5f778f002f
damarvijis <damar.j@mail.ugm.ac.id>
2023-10-11 23:54:00 +0700

    minify image

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
 const path = require("path");
+const glob = require("glob");
 const { merge } = require('webpack-merge');
 const config = require("./webpack.base")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin"); // minify css (yg kepake doang)
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin"); // minify image
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = merge(config, { // cara 2 buat gabungin config
   mode: 'production',
@@ -11,10 +19,24 @@ module.exports = merge(config, { // cara 2 buat gabungin config
     assetModuleFilename: "img/[name]-[hash][ext]", // hash buat caching
     clean: true // bersihin folder dist setiap generate baru.
   },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+        },
+      }),
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "main.[contenthash].css"
-    })
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
   ]
 });
 // module.exports = { cara 1 buat gabungin config
